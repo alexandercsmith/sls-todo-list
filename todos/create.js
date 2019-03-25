19901b0fd5bed4e0beac819c27faafe85916a41d
Alexander Smith <acsmith.dev@gmail.com>
2019-03-25 12:21:29 -0400

    update create function for todos

--- todos/create.js
+++ todos/create.js
@@ -1,12 +1,17 @@
+const db = require('../db.js');
+
 module.exports.createTodo = (event, context, callback) => {
   const body = JSON.parse(event.body);
 
-  const mockDb = body.todo + ' is now saved to the Database.';
-
-  return callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      todo: mockDb
+  db.todo
+    .create({
+      task: body.task
+    }).then(todo => {
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          todo: todo
+        })
+      })
     })
-  })
 }
