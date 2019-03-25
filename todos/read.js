b7df7804f490f47275cfe4a110f87842a0ee1cf5
Alexander Smith <acsmith.dev@gmail.com>
2019-03-25 12:35:50 -0400

    add list todos route

--- todos/read.js
+++ todos/read.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const db = require('../db.js');
+
 module.exports.getTodo = async (event, context, callback) => {
   const todo = "Make Dinner"
   const response = {
@@ -6,5 +10,19 @@ module.exports.getTodo = async (event, context, callback) => {
       todo: todo
     })
   }
+  callback(null, response)
+}
+
+module.exports.listTodos = async (event, context, callback) => {
+  db.todo.findAll({
+    attributes: ['id', 'task', 'completed']
+  }).then(todos => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        todos: todos
+      })
+    }
+  });
   callback(null, response)
 }
